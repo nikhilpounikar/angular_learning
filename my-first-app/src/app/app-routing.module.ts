87cd81deba34ebf291764031f0e49077f798a461
nikhilpounikar <nikhilpounikar@gmail.com>
2023-12-29 11:04:56 +0530

    update route added

--- my-first-app/src/app/app-routing.module.ts
+++ my-first-app/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentListComponent } from './components/student-list/student-list.component';
 import { AddStudentComponent } from './components/add-student/add-student.component';
+import { UpdateStudentComponent } from './components/update-student/update-student.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: StudentListComponent },
   { path: 'add-student', component: AddStudentComponent },
+  { path: 'update-student/:id', component: UpdateStudentComponent }
 ];
 
 
--- my-first-app/src/app/components/update-student/update-student.component.ts
+++ my-first-app/src/app/components/update-student/update-student.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from 'src/app/models/student.model';
+import { StudentService } from 'src/app/services/student.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-update-student',
+  selector: 'update-student',
   templateUrl: './update-student.component.html',
   styleUrls: ['./update-student.component.scss']
 })
-export class UpdateStudentComponent {
+export class UpdateStudentComponent implements OnInit {
+
+
+  student!: Student;
+
+  private studentId!:number;
+  constructor(private studentService: StudentService,private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+     // Retrieve student ID from route parameters
+
+     const param = this.route.snapshot.paramMap.get('id');
+     if(param != null)
+      this.studentId = parseInt(param);
+     else 
+      this.studentId = -1;
+
+     console.log('Student ID:', this.studentId);
+  }
+
+  updateStudent(): void {
+    this.studentService.studentListLengthObservable.subscribe((length)=>{
+      this.student.id = length + 1;
+    })
+    this.studentService.addStudent(this.student);
+    this.student = new Student(0, '', '', 0); // Clear the form
+  }
 
 }
