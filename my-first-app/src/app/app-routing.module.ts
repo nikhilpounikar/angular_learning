d68747a37535d5d26cad6fdb267f1857e7fc3c80
nikhilpounikar <nikhilpounikar@gmail.com>
2024-01-02 11:53:02 +0530

    add-course component set-up

--- my-first-app/src/app/app-routing.module.ts
+++ my-first-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { StudentListComponent } from './components/student-list/student-list.component';
 import { AddStudentComponent } from './components/add-student/add-student.component';
 import { UpdateStudentComponent } from './components/update-student/update-student.component';
+import { AddCourseComponent } from './components/add-course/add-course.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: StudentListComponent },
   { path: 'add-student', component: AddStudentComponent },
-  { path: 'update-student/:id', component: UpdateStudentComponent }
+  { path: 'update-student/:id', component: UpdateStudentComponent },
+  { path: 'add-course', component: AddCourseComponent },
 ];
 
 
--- my-first-app/src/app/components/add-course/add-course.component.ts
+++ my-first-app/src/app/components/add-course/add-course.component.ts
@@ -4,18 +4,35 @@ import { Course } from 'src/app/store/models/course.model';
 @Component({
   selector: 'add-course',
   templateUrl: './add-course.component.html',
-  styleUrls: ['./add-course.component.scss']
+  styleUrls: ['./add-course.component.scss'],
 })
 export class AddCourseComponent {
+  newCourse!: Course;
 
-    newCourse!:Course;
+  constructor() {
+    this.newCourse = {
+      id: this.generateCourseID(),
+      name: '',
+      price: 999,
+      currency: 'INR',
+      description: '',
+    };
+  }
 
-    constructor() {
-      
+  addCourse(): void {
+    console.log('Adding Course');
+  }
+
+  private generateCourseID():string{
+    const length = 12; // You can adjust the length as needed
+    const characters = 'COURSE-';
+    let result = '';
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
     }
-
-    addCourse():void{
-      console.log('Adding Course');
-    }
-
+  
+    return result;
+  }
 }
