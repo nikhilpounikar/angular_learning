eaf603cce87bfb8ce55ff39382816143d6a18d8f
nikhilpounikar <nikhilpounikar@gmail.com>
2024-01-08 17:23:27 +0530

    guard added

--- student-manager/src/app/app.routes.ts
+++ student-manager/src/app/app.routes.ts
@@ -1,19 +1,18 @@
 import { Routes } from '@angular/router';
 import { PublicLayoutComponent } from './layout/public/public-layout/public-layout.component';
 import { PrivateLayoutComponent } from './layout/private/private-layout/private-layout.component';
-import { CanActivateViaAuthGuard } from './guards/authenticated';
-import { NotFoundError } from 'rxjs';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { LoginComponent } from './components/public/login/login.component';
 import { SignUpComponent } from './components/public/sign-up/sign-up.component';
+import { UserAuthenticatedGuard } from './guards/authenticated';
 
 export const routes: Routes = [
 
   {
     path: '',
     component: PrivateLayoutComponent,
-    canActivate: [CanActivateViaAuthGuard],
-    // canActivate: [CanActivateViaAuthGuard],
+    // canActivate: [userAuthenticatedGuard],
+    canActivate: [UserAuthenticatedGuard],
   },
   {
     path: '',
--- student-manager/src/app/guards/authenticated.ts
+++ student-manager/src/app/guards/authenticated.ts
@@ -1,16 +1,33 @@
-import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { Injectable, inject } from '@angular/core';
+import {  CanActivateFn } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState, } from '../reducers/index';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 
-@Injectable()
-export class CanActivateViaAuthGuard implements CanActivate {
+// @Injectable()
+// class UserAuthenticationService {
+//   canActivate(): boolean {
+//     return false;
+//   }
 
-  constructor(private store: Store<AppState>) {}
+// }
 
-  canActivate() {
-    // return this.store.select(getUserAuthStatus);
+// export const userAuthenticatedGuard: CanActivateFn =  (route, state) => {
+//   return inject(UserAuthenticationService).canActivate();
+// };
 
-    return true;
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserAuthenticatedGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    // Your guard logic here
+    return true; // or false based on your conditions
   }
 }
