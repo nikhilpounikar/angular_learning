d099ce2f6a1d19936c77908951452750b11a77e8
nikhilpounikar <nikhilpounikar@gmail.com>
2024-01-09 13:45:42 +0530

    small change

--- student-manager/src/app/app.component.ts
+++ student-manager/src/app/app.component.ts
@@ -1,14 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { DBConfig, NgxIndexedDBModule } from 'ngx-indexed-db';
-
-const dbConfig: DBConfig = {
-  name: 'student-DB',
-  version: 1,
-  objectStoresMeta: [],
-};
-
 
 @Component({
   selector: 'app-root',
